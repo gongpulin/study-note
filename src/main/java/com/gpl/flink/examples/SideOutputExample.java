72dc153d4277238b04eaacc24177de644c57413b
gongpulin <gongpl@ehualu.com>
2020-07-09 21:37:30 +0800

    leetcode接雨水

---
+++ src/main/java/com/gpl/flink/examples/SideOutputExample.java
@@ -0,0 +1,8 @@
+package com.gpl.flink.examples;
+
+/**
+ * @author gongpulin
+ * date 2020-07-09
+ */
+public class SideOutputExample {
+}
--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P42TrappingRainWater.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P42TrappingRainWater.java
@@ -50,6 +50,37 @@ class Solution {
         return area;
 
     }
+
+
+
+    public int trap1(int[] height) {
+        int area = 0;
+        if (height == null || height.length == 0) {
+            return area;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int left_max = 0;
+        int right_max = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                left_max = left_max > height[left] ? left_max : height[left];
+                if (left_max > height[left]) {
+                    area = area + left_max - height[left];
+                }
+                left++;
+            } else {
+                right_max = right_max > height[right] ? right_max : height[right];
+                if (right_max > height[right]) {
+                    area = area + right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return area;
+    }
+
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
