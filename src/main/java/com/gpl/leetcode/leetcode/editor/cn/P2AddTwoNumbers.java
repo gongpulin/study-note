7a7f5dc40960d51834e3e87dac3413c7d43f2278
gongpulin <gongpl@ehualu.com>
2020-07-09 21:41:24 +0800

    两数相加

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P2AddTwoNumbers.java
@@ -0,0 +1,64 @@
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。 
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 示例： 
+//
+// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//输出：7 -> 0 -> 8
+//原因：342 + 465 = 807
+// 
+// Related Topics 链表 数学
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：两数相加
+public class P2AddTwoNumbers{
+    public static void main(String[] args) {
+        Solution solution = new P2AddTwoNumbers().new Solution();
+        // TO TEST
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode aHead = new ListNode(0);
+        ListNode p = l1, q = l2, curNode = aHead;
+        int carry = 0;//进位
+        while (p != null || q != null) {
+            int x = p != null ? p.val : 0;
+            int y = q != null ? q.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            curNode.next = node;
+            curNode = curNode.next;
+            p = p != null ? p.next : null;
+            q = q != null ? q.next : null;
+
+        }
+        if (carry != 0) {
+            ListNode n = new ListNode(carry);
+            curNode.next = n;
+        }
+        return aHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
