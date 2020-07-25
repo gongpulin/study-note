73fc804a0b45ee29d8b41f7524f55405628eb18e
gongpulin <gongpl@ehualu.com>
2020-07-25 12:01:26 +0800

    移除链表倒数第N个节点

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P19RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P19RemoveNthNodeFromEndOfList.java
@@ -48,6 +48,26 @@ class Solution {
         second.next = second.next.next;
         return dummy.next;
     }
+
+
+
+
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
