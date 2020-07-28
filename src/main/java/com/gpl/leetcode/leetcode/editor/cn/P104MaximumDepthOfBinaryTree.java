70ffe05c23266af65752a31e8c0dbac9a7a8a138
gongpulin <gongpl@ehualu.com>
2020-07-28 21:13:22 +0800

    树最大深度

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,90 @@
+//给定一个二叉树，找出其最大深度。 
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Java：二叉树的最大深度
+public class P104MaximumDepthOfBinaryTree{
+    public static void main(String[] args) {
+        Solution solution = new P104MaximumDepthOfBinaryTree().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if ( root == null ) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans = 0;
+        while ( !queue.isEmpty() ) {
+            int size = queue.size();
+            while ( size > 0 ) {
+                TreeNode node = queue.poll();
+//                System.out.println(node.val);
+                if ( node.left != null ) {
+                    queue.offer(node.left);
+                }
+                if ( node.right != null ) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+
+
+//    public int maxDepth(TreeNode root) {
+//        if ( root == null ) {
+//            return 0;
+//        }
+//        if (root.left == null && root.right == null ) {
+//            return 1;
+//        }
+//        int leftMaxDepth = maxDepth(root.left) + 1;
+//        int rightMaxDepth = maxDepth(root.right) + 1;
+//        return leftMaxDepth > rightMaxDepth ? leftMaxDepth : rightMaxDepth;
+//
+//    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
