8e1c28b32cd87b877517f16f463a054b54923cd4
gongpulin <gongpl@ehualu.com>
2020-07-30 20:22:33 +0800

    平衡树

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P110BalancedBinaryTree.java
@@ -0,0 +1,85 @@
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。 
+// 
+//
+// 示例 1: 
+//
+// 给定二叉树 [3,9,20,null,null,15,7] 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回 true 。 
+// 
+//示例 2: 
+//
+// 给定二叉树 [1,2,2,3,3,null,null,4,4] 
+//
+//        1
+//      / \
+//     2   2
+//    / \
+//   3   3
+//  / \
+// 4   4
+// 
+//
+// 返回 false 。 
+//
+// 
+// Related Topics 树 深度优先搜索
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：平衡二叉树
+public class P110BalancedBinaryTree{
+    public static void main(String[] args) {
+        Solution solution = new P110BalancedBinaryTree().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+
+    }
+    private int maxDepth(TreeNode node) {
+        if ( node == null ) {
+            return 0;
+        }
+        if ( node.left == null && node.right == null ) {
+            return 1;
+        }
+        int leftMaxDepth = maxDepth(node.left);
+        int rightMaxDepth = maxDepth(node.right);
+        if (leftMaxDepth == -1 || rightMaxDepth == -1 || Math.abs(leftMaxDepth-rightMaxDepth) > 1) {
+            return -1;
+        }
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+}
