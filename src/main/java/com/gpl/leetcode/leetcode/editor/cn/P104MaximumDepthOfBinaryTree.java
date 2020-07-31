f1a4bfa09cad51785a113dc19bd901d8ad9e0ee3
gongpulin <gongpl@ehualu.com>
2020-07-31 22:37:15 +0800

    二叉树最大深度

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P104MaximumDepthOfBinaryTree.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P104MaximumDepthOfBinaryTree.java
@@ -78,6 +78,30 @@ class Solution {
 //        return leftMaxDepth > rightMaxDepth ? leftMaxDepth : rightMaxDepth;
 //
 //    }
+
+    public int maxDepth3(TreeNode root) {
+        if ( root == null ) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int ans = 0;
+        while ( !queue.isEmpty() ) {
+            int size = queue.size();
+            while ( size > 0 ) {
+                TreeNode node = queue.poll();
+                if ( node.left != null ) {
+                    queue.offer(node.left);
+                }
+                if ( node.right != null ) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
 }
 class TreeNode {
     int val;
