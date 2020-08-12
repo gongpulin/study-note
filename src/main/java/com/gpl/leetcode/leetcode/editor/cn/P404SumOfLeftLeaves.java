50299491f8574778dd9aa076984311a849a4268d
gongpulin <gongpl@ehualu.com>
2020-08-12 21:54:23 +0800

    左叶子节点之和

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P404SumOfLeftLeaves.java
@@ -0,0 +1,58 @@
+//计算给定二叉树的所有左叶子之和。 
+//
+// 示例： 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24 
+//
+// 
+// Related Topics 树
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：左叶子之和
+public class P404SumOfLeftLeaves{
+    public static void main(String[] args) {
+        Solution solution = new P404SumOfLeftLeaves().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            ans = ans + root.left.val;
+        }
+        sumOfLeftLeaves(root.left);
+        sumOfLeftLeaves(root.right);
+        return ans;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+}
