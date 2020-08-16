8ae9b057e276ff6eb6838d8f36533cd5df2db6e6
gongpulin <gongpl@ehualu.com>
2020-08-16 21:59:52 +0800

    最小深度

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P111MinimumDepthOfBinaryTree.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P111MinimumDepthOfBinaryTree.java
@@ -19,6 +19,10 @@
 
 
 package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 //Java：二叉树的最小深度
 public class P111MinimumDepthOfBinaryTree{
     public static void main(String[] args) {
@@ -36,22 +40,53 @@ public class P111MinimumDepthOfBinaryTree{
  * }
  */
 class Solution {
+
+    //BFS 广度优先遍历
     public int minDepth(TreeNode root) {
-        if ( root == null ) {
+        if (root == null) {
             return 0;
         }
-        if ( root.left == null && root.right == null) {
-            return 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            depth++;
         }
-        int MIN_DEPTH = Integer.MAX_VALUE;
-        if ( root.left != null ) {
-            MIN_DEPTH = Math.min(MIN_DEPTH, minDepth(root.left));
-        }
-        if ( root.right != null ) {
-            MIN_DEPTH = Math.min(MIN_DEPTH, minDepth(root.right));
-        }
-        return MIN_DEPTH + 1;
+        return depth;
     }
+
+
+    //深度优先遍历
+//    public int minDepth(TreeNode root) {
+//        if ( root == null ) {
+//            return 0;
+//        }
+//        if ( root.left == null && root.right == null) {
+//            return 1;
+//        }
+//        int MIN_DEPTH = Integer.MAX_VALUE;
+//        if ( root.left != null ) {
+//            MIN_DEPTH = Math.min(MIN_DEPTH, minDepth(root.left));
+//        }
+//        if ( root.right != null ) {
+//            MIN_DEPTH = Math.min(MIN_DEPTH, minDepth(root.right));
+//        }
+//        return MIN_DEPTH + 1;
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 class TreeNode {
