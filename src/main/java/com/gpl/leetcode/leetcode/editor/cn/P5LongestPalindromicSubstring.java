275163b177af90e59f9872b8c346e649d124f0f0
gongpulin <gongpl@ehualu.com>
2020-08-20 09:29:47 +0800

    最长回文子串

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -58,18 +58,38 @@ class Solution {
     }
 
 
-    private String centerSpread1(String s, int left, int right) {
-        int i = left, j = right;
+
+    public String longestPalindrome1(String s) {
+        if ( s == null || s.length() == 0 ) {
+            return "";
+        }
         int len = s.length();
-        while (i >= 0 && j < len) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i--;
-                j++;
+        if ( len < 2 ) {
+            return s;
+        }
+        String ans = s.substring(0, 1);
+        for ( int i = 1; i < len; i++ ) {
+            String oddStr = centerSpread1(s, i, i+1);
+            String evenStr = centerSpread1(s, i, i);
+            String maxLengthStr = oddStr.length() > evenStr.length() ? oddStr : evenStr;
+            if ( maxLengthStr.length() > ans.length() ) {
+                ans = maxLengthStr;
+            }
+        }
+        return ans;
+    }
+
+    private String centerSpread1(String s, int left, int right) {
+        int len = s.length();
+        while ( left >= 0 && right < len ) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
             } else {
                 break;
             }
         }
-        return s.substring(i+1,j);
+        return s.substring(left+1, right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
