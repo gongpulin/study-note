847197ed99e27a846fc54d7f7cf9a70825b8591e
gongpulin <gongpl@ehualu.com>
2020-08-21 09:12:02 +0800

    第k小元素

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P230KthSmallestElementInABst.java
@@ -0,0 +1,111 @@
+//给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。 
+//
+// 说明： 
+//你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。 
+//
+// 示例 1: 
+//
+// 输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 1 
+//
+// 示例 2: 
+//
+// 输入: root = [5,3,6,2,4,null,null,1], k = 3
+//       5
+//      / \
+//     3   6
+//    / \
+//   2   4
+//  /
+// 1
+//输出: 3 
+//
+// 进阶： 
+//如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？ 
+// Related Topics 树 二分查找
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//Java：二叉搜索树中第K小的元素
+public class P230KthSmallestElementInABst{
+    public static void main(String[] args) {
+        Solution solution = new P230KthSmallestElementInABst().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest1(TreeNode root, int k) {
+        if (root == null) {
+            return -1;
+        }
+        Stack<TreeNode> stack  = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                k--;
+                if (k == 0) {
+                    return cur.val;
+                }
+                cur = cur.right;
+            }
+        }
+        return -1;
+    }
+
+
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return -1;
+        }
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root, list);
+        return list.get(k - 1);
+    }
+    private void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+}
