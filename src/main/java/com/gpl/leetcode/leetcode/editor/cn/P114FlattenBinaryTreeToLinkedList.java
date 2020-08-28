2bed641ba801f028d18bbeb26d9d54d0f47d29a3
gongpulin <gongpl@ehualu.com>
2020-08-28 09:15:33 +0800

    二叉树转化为链表

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P114FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,105 @@
+//给定一个二叉树，原地将它展开为一个单链表。 
+//
+// 
+//
+// 例如，给定二叉树 
+//
+//     1
+//   / \
+//  2   5
+// / \   \
+//3   4   6 
+//
+// 将其展开为： 
+//
+// 1
+// \
+//  2
+//   \
+//    3
+//     \
+//      4
+//       \
+//        5
+//         \
+//          6 
+// Related Topics 树 深度优先搜索
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+//Java：二叉树展开为链表
+public class P114FlattenBinaryTreeToLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new P114FlattenBinaryTreeToLinkedList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public void flatten(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            List<TreeNode> list = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+            while(!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                list.add(node);
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+            for (int i = 1; i < list.size(); i++) {
+                TreeNode pre = list.get(i-1);
+                TreeNode next = list.get(i);
+                pre.left = null;
+                pre.right = next;
+            }
+
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
