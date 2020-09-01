d4f741d5bf01b35ac326741c6c63deb81bfe7681
gongpulin <gongpl@ehualu.com>
2020-09-01 08:54:43 +0800

    二叉树后续遍历

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P145BinaryTreePostorderTraversal.java
@@ -0,0 +1,87 @@
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.*;
+
+//Java：二叉树的后序遍历
+public class P145BinaryTreePostorderTraversal{
+    public static void main(String[] args) {
+        Solution solution = new P145BinaryTreePostorderTraversal().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        List<Integer> ans = new ArrayList<>();
+//        if (root == null) {
+//            return ans;
+//        }
+//        travelsal(root, ans);
+//        return ans;
+//
+//    }
+//    private void travelsal(TreeNode root, List<Integer> ans) {
+//        if (root == null) {
+//            return;
+//        }
+//        travelsal(root.left, ans);
+//        travelsal(root.right, ans);
+//        ans.add(root.val);
+//    }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ans.offerFirst(node.val);
+            if (node.left != null) {   //注意这里和中序遍历不同，先左孩子入栈   依次遍历 根->右->左  再反转就是后续遍历
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return ans;
+    }
+
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+}
