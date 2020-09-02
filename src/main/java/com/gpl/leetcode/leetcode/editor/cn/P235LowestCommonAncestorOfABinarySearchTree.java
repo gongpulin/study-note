b5491818024b601235c4fd2f5c6e75e7bfe68bfd
gongpulin <gongpl@ehualu.com>
2020-09-02 08:54:31 +0800

    最近公共祖先

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P235LowestCommonAncestorOfABinarySearchTree.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P235LowestCommonAncestorOfABinarySearchTree.java
@@ -62,19 +62,6 @@ class Solution {
         }
     }
 
-//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//        TreeNode node = root;
-//        while (node != null) {
-//            if (node.val > p.val && node.val > q.val) {
-//                node = node.left;
-//            } else if (node.val < p.val && node.val < q.val) {
-//                node = node.right;
-//            } else {
-//                return node;
-//            }
-//        }
-//        return null;
-//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 class TreeNode {
