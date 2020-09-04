24ac73fb2c36d93b4d305d7cebcfd03c0fb89a58
gongpulin <gongpl@ehualu.com>
2020-09-04 09:31:49 +0800

    最长无重复子串

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P3LongestSubstringWithoutRepeatingCharacters.java
@@ -49,17 +49,17 @@ public class P3LongestSubstringWithoutRepeatingCharacters{
     class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-        int rk = -1, res = 0;
+        int rk = 0, res = 0;
         Set<Character> set = new HashSet<Character>();
         for (int i = 0; i < n; i ++) {
             if ( i != 0 ) {
                 set.remove(s.charAt(i-1));
             }
-            while ( rk + 1 < n && !set.contains(s.charAt(rk + 1))) {
-                set.add(s.charAt(rk+1));
+            while ( rk < n && !set.contains(s.charAt(rk))) {
+                set.add(s.charAt(rk));
                 rk++;
             }
-            res = Math.max(res,rk-i+1);
+            res = Math.max(res,rk-i);
         }
         return res;
     }
