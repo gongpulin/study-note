d52c2984e646765852c8838e539ef5ca4680bd67
gongpulin <gongpl@ehualu.com>
2020-09-06 14:50:26 +0800

    下一个排列

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P31NextPermutation.java
@@ -0,0 +1,62 @@
+//实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。 
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。 
+//
+// 必须原地修改，只允许使用额外常数空间。 
+//
+// 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。 
+//1,2,3 → 1,3,2 
+//3,2,1 → 1,2,3 
+//1,1,5 → 1,5,1 
+// Related Topics 数组
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+//Java：下一个排列
+public class P31NextPermutation{
+    public static void main(String[] args) {
+        Solution solution = new P31NextPermutation().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null && nums.length == 0) {
+            return;
+        }
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i+1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while(i >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1);
+
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int[] nums, int start) {
+        int left = start;
+        int right = nums.length-1;
+        while(left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
