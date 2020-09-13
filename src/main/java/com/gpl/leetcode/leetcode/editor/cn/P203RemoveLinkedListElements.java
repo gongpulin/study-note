c11633454e6e2f40bda3fce34b0c8b846a300797
gongpulin <gongpl@ehualu.com>
2020-09-13 22:02:59 +0800

    移除给定值元素

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P203RemoveLinkedListElements.java
@@ -0,0 +1,54 @@
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：移除链表元素
+public class P203RemoveLinkedListElements{
+    public static void main(String[] args) {
+        Solution solution = new P203RemoveLinkedListElements().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while(cur.next != null) {
+            if(cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+}
