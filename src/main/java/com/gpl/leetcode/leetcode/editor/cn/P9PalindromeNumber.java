640e4279533f2893a5a317be0e1c52e00b5b47d2
gongpulin <gongpl@ehualu.com>
2020-09-24 23:14:49 +0800

    回文数

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P9PalindromeNumber.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P9PalindromeNumber.java
@@ -36,6 +36,27 @@ public class P9PalindromeNumber{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 
+        public boolean isPalindrome2(int x) {
+            if (x < 0) {
+                return false;
+            }
+            int div = 1;
+            while (x / div >= 10) {
+                div = div * 10;
+            }
+            while (x > 0) {
+                int left = x / div;
+                int right = x % 10;
+                if (left != right) {
+                    return false;
+                }
+                x = (x % div) / 10;
+                div = div / 100;
+            }
+            return true;
+        }
+
+
 
     public boolean isPalindrome(int x) {
         if (x < 0) {
