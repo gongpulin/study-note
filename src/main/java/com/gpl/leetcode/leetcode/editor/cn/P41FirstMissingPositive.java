190a7fa7e53b5249de8d10e03826f23f8d710905
gongpulin <gongpl@ehualu.com>
2020-10-26 09:11:33 +0800

    缺失的第一个正数

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P41FirstMissingPositive.java
@@ -0,0 +1,69 @@
+//给你一个未排序的整数数组，请你找出其中没有出现的最小的正整数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [1,2,0]
+//输出: 3
+// 
+//
+// 示例 2: 
+//
+// 输入: [3,4,-1,1]
+//输出: 2
+// 
+//
+// 示例 3: 
+//
+// 输入: [7,8,9,11,12]
+//输出: 1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的额外空间。 
+// Related Topics 数组
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：缺失的第一个正数
+public class P41FirstMissingPositive{
+    public static void main(String[] args) {
+        Solution solution = new P41FirstMissingPositive().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+        /**
+         * 对于一个长度为N的数组，没有出现的最小正整数只能在[1,N+1]中，
+         */
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);//可能已经出现过，nums[i]为负数，所以abs
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
