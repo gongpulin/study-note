07e04101c7ac0655dfde779325adf7c467bb9a84
gongpulin <gongpl@ehualu.com>
2020-11-10 09:15:54 +0800

    合并K个升序链表

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P23MergeKSortedLists.java
@@ -0,0 +1,166 @@
+//给你一个链表数组，每个链表都已经按升序排列。 
+//
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+//输出：[1,1,2,3,4,4,5,6]
+//解释：链表数组如下：
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//将它们合并到一个有序链表中得到。
+//1->1->2->3->4->4->5->6
+// 
+//
+// 示例 2： 
+//
+// 输入：lists = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 输入：lists = [[]]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// k == lists.length 
+// 0 <= k <= 10^4 
+// 0 <= lists[i].length <= 500 
+// -10^4 <= lists[i][j] <= 10^4 
+// lists[i] 按 升序 排列 
+// lists[i].length 的总和不超过 10^4 
+// 
+// Related Topics 堆 链表 分治算法
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+//Java：合并K个升序链表
+public class P23MergeKSortedLists{
+    public static void main(String[] args) {
+        Solution solution = new P23MergeKSortedLists().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(new Comparator<ListNode> (){
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.offer(node);
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            p.next = node;
+            p = p.next;
+            if (node.next != null) {
+                queue.offer(node.next);
+            }
+        }
+        return dummy.next;
+    }
+
+
+
+
+    private ListNode mergeTwoLists(ListNode p1, ListNode p2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p.next = p1 != null ? p1 : p2;
+        return dummy.next;
+    }
+
+    //方案二，二分
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        return mergeTwoLists(merge(lists, left, mid), merge(lists, mid + 1, right));
+    }
+
+
+
+
+
+
+
+    //方案三：两两合并
+    public ListNode mergeKLists3(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        int len = lists.length;
+        ListNode ans = lists[0];
+        for (int i = 1; i < len; i++) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+}
