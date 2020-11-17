5602b6152e78d67e37af5035dc9d7cbce61b98ff
gongpulin <gongpl@ehualu.com>
2020-11-17 21:47:30 +0800

    多数元素

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P169MajorityElement.java
@@ -0,0 +1,43 @@
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [3,2,3]
+//输出: 3 
+//
+// 示例 2: 
+//
+// 输入: [2,2,1,1,1,2,2]
+//输出: 2
+// 
+// Related Topics 位运算 数组 分治算法
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：多数元素
+public class P169MajorityElement{
+    public static void main(String[] args) {
+        Solution solution = new P169MajorityElement().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
