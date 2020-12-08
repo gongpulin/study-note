30db95ece39763cae8c336fb411e6f1017832d44
gongpulin <gongpulin@163.com>
2020-12-08 09:16:43 +0800

    完美数

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P507PerfectNumber.java
@@ -0,0 +1,73 @@
+//对于一个 正整数，如果它和除了它自身以外的所有 正因子 之和相等，我们称它为 「完美数」。 
+//
+// 给定一个 整数 n， 如果是完美数，返回 true，否则返回 false 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：28
+//输出：True
+//解释：28 = 1 + 2 + 4 + 7 + 14
+//1, 2, 4, 7, 和 14 是 28 的所有正因子。 
+//
+// 示例 2： 
+//
+// 输入：num = 6
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 输入：num = 496
+//输出：true
+// 
+//
+// 示例 4： 
+//
+// 输入：num = 8128
+//输出：true
+// 
+//
+// 示例 5： 
+//
+// 输入：num = 2
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= num <= 108 
+// 
+// Related Topics 数学
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：完美数
+public class P507PerfectNumber{
+    public static void main(String[] args) {
+        Solution solution = new P507PerfectNumber().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        int len = (int)Math.sqrt(num);
+        int ans = 1;
+        for (int i = 2; i <= len; i++) {
+            if (num % i == 0) {
+                ans += i + num / i;
+            }
+        }
+        return ans == num;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
