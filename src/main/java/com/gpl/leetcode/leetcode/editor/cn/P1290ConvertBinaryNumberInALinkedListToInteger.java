85a90d28827c16fee208cd5535e7e8d828b06624
gongpulin <gongpulin@163.com>
2020-12-15 09:22:15 +0800

    二进制链表转整数

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P1290ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,106 @@
+//给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。 
+//
+// 请你返回该链表所表示数字的 十进制值 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 输入：head = [1,0,1]
+//输出：5
+//解释：二进制数 (101) 转化为十进制数 (5)
+// 
+//
+// 示例 2： 
+//
+// 输入：head = [0]
+//输出：0
+// 
+//
+// 示例 3： 
+//
+// 输入：head = [1]
+//输出：1
+// 
+//
+// 示例 4： 
+//
+// 输入：head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+//输出：18880
+// 
+//
+// 示例 5： 
+//
+// 输入：head = [0,0]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表不为空。 
+// 链表的结点总数不超过 30。 
+// 每个结点的值不是 0 就是 1。 
+// 
+// Related Topics 位运算 链表
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.Stack;
+
+//Java：二进制链表转整数
+public class P1290ConvertBinaryNumberInALinkedListToInteger{
+    public static void main(String[] args) {
+        Solution solution = new P1290ConvertBinaryNumberInALinkedListToInteger().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        ListNode cur = head;
+        int ans = 0;
+        while (cur != null) {
+            ans = ans * 2 + cur.val;
+            cur = cur.next;
+        }
+        return ans;
+    }
+    public int getDecimalValue1(ListNode head) {
+        ListNode node = head;
+        int len = getListLen(head);
+        int ans = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            ans += node.val * Math.pow(2, i);
+            node = node.next;
+        }
+        return ans;
+    }
+    private int getListLen(ListNode node) {
+        int ans = 0;
+        while (node != null) {
+            ans++;
+            node = node.next;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+}
