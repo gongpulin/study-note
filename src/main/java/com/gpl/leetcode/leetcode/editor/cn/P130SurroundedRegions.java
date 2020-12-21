bbdf538e0991c7dda4c66add3ba0849d55c4f19c
gongpulin <gongpulin@163.com>
2020-12-21 22:48:33 +0800

    被围绕的区域

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P130SurroundedRegions.java
@@ -0,0 +1,74 @@
+//给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。 
+//
+// 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。 
+//
+// 示例: 
+//
+// X X X X
+//X O O X
+//X X O X
+//X O X X
+// 
+//
+// 运行你的函数后，矩阵变为： 
+//
+// X X X X
+//X X X X
+//X X X X
+//X O X X
+// 
+//
+// 解释: 
+//
+// 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被
+//填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。 
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：被围绕的区域
+public class P130SurroundedRegions{
+    public static void main(String[] args) {
+        Solution solution = new P130SurroundedRegions().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        if (m == 0) {
+            return;
+        }
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+    private void dfs(char[][] board, int x, int y) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x + 1, y);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x, y - 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
