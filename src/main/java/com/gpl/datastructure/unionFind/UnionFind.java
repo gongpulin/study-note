8d2888205fbb626e7162a291604b68fed8c7575e
gongpulin <gongpulin@163.com>
2020-12-23 23:29:22 +0800

    并查集1

---
+++ src/main/java/com/gpl/datastructure/unionFind/UnionFind.java
@@ -0,0 +1,51 @@
+package com.gpl.datastructure.unionFind;
+
+/**
+ * @author gongpulin
+ * date 2020-12-21
+ */
+
+/**
+ * 数组实现并查集，元素内数字代表集合号
+ */
+public class UnionFind {
+    /**
+     * 数组表示并查集所有元素
+     */
+    private int[] id;
+
+    /**
+     * 并查集元素的个数
+     */
+    private int size;
+
+    public UnionFind(int size) {
+        this.size = size;
+        id = new int[size];
+        for (int i = 0; i < size; i++) {
+            id[i] = i;
+        }
+    }
+
+    public int find(int element) {
+        return id[element];
+    }
+
+    public boolean isConnected(int e1, int e2) {
+        return find(e1) == find(e2);
+    }
+
+    public void unionElements(int e1, int e2) {
+        int firstUnion = find(e1);
+        int secondUnion = find(e2);
+        if (firstUnion != secondUnion) {
+            for (int i = 0; i < size; i++) {
+                if (id[i] == firstUnion) {
+                    id[i] = secondUnion;
+                }
+            }
+        }
+    }
+
+
+}
