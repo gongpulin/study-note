0031eb639578a06aabb142cff112c3ff1926e159
gongpulin <gongpulin@163.com>
2020-12-29 09:10:11 +0800

    最长回文子串

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -24,7 +24,7 @@ public class P5LongestPalindromicSubstring{
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public String longestPalindrome(String s) {
+    public String longestPalindrome1(String s) {
         if (s == null || s.length() < 1) {
             return "";
         }
@@ -58,39 +58,38 @@ class Solution {
     }
 
 
-
-    public String longestPalindrome1(String s) {
+        /**
+         * dp[i][j]表示s[i:j]是否为回文串，若dp[i][j]为回文串，则dp[i+1][j-1]也为回文串并且s[i]=s[j]
+         */
+    public String longestPalindrome(String s) {
         if ( s == null || s.length() == 0 ) {
             return "";
         }
         int len = s.length();
-        if ( len < 2 ) {
-            return s;
-        }
-        String ans = s.substring(0, 1);
-        for ( int i = 1; i < len; i++ ) {
-            String oddStr = centerSpread1(s, i, i+1);
-            String evenStr = centerSpread1(s, i, i);
-            String maxLengthStr = oddStr.length() > evenStr.length() ? oddStr : evenStr;
-            if ( maxLengthStr.length() > ans.length() ) {
-                ans = maxLengthStr;
+        boolean[][] dp = new boolean[len][len];
+        String ans = "";
+        for (int l = 0; l < len; l++) {
+            for (int i = 0; i + l < len; i++) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
             }
         }
         return ans;
+
+
+
     }
 
-    private String centerSpread1(String s, int left, int right) {
-        int len = s.length();
-        while ( left >= 0 && right < len ) {
-            if (s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-            } else {
-                break;
-            }
-        }
-        return s.substring(left+1, right);
-    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
