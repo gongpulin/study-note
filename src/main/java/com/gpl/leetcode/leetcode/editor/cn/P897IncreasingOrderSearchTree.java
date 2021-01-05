df84212e578162df1d78f94d77406937d9c73ac0
gongpulin <gongpulin@163.com>
2021-01-05 09:23:00 +0800

    递增顺序查找树

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P897IncreasingOrderSearchTree.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P897IncreasingOrderSearchTree.java
@@ -73,27 +73,69 @@ public class P897IncreasingOrderSearchTree{
  * }
  */
 class Solution {
+    TreeNode cur;
     public TreeNode increasingBST(TreeNode root) {
-        List<TreeNode> nodes = new ArrayList();
-        dfs(root, nodes);
         TreeNode head = new TreeNode();
-        TreeNode next = head;
-        for (TreeNode node : nodes) {
-            next.left = null;
-            next.right = node;
-            next = node;
-        }
+        cur = head;
+        dfs(root);
         return head.right;
     }
-    private void dfs(TreeNode node, List<TreeNode> list) {
+    private void dfs(TreeNode node) {
         if (node == null) {
             return;
         }
-        dfs(node.left, list);
-        list.add(node);
-        dfs(node.right, list);
-
+        dfs(node.left);
+        cur.right = node;
+        node.left = null;
+        cur = node;
+        dfs(node.right);
     }
+
+//    public TreeNode increasingBST(TreeNode root) {
+//        List<Integer> values = new ArrayList();
+//        dfs(root, values);
+//        TreeNode head = new TreeNode();
+//        TreeNode cur = head;
+//        for (int val : values) {
+//            TreeNode node = new TreeNode(val);
+//            cur.right = node;
+//            cur.left = null;
+//            cur = cur.right;
+//        }
+//        return head.right;
+//    }
+//    private void dfs(TreeNode node, List<Integer> list) {
+//        if (node == null) {
+//            return;
+//        }
+//        dfs(node.left, list);
+//        list.add(node.val);
+//        dfs(node.right, list);
+//
+//    }
+
+    //错误答案   Error Found cycle in the TreeNode
+//    public TreeNode increasingBST(TreeNode root) {
+//        List<TreeNode> nodes = new ArrayList();
+//        dfs(root, nodes);
+//        TreeNode head = new TreeNode();
+//        TreeNode cur = head;
+//        for (TreeNode node : nodes) {
+//            cur.left = null;
+//            cur.right = node;
+//            cur = cur.right;
+//        }
+//        return head.right;
+//    }
+//    private void dfs(TreeNode node, List<TreeNode> list) {
+//        if (node == null) {
+//            return;
+//        }
+//        dfs(node.left, list);
+//        list.add(node);
+//        dfs(node.right, list);
+//
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 public class TreeNode {
