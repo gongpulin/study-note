d53b587a773a79ebf4b75330232ebcf615ed5895
gongpulin <gongpulin@163.com>
2021-01-05 09:12:20 +0800

    最长回文子序列

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P516LongestPalindromicSubsequence.java
@@ -0,0 +1,80 @@
+//给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。 
+//
+// 
+//
+// 示例 1: 
+//输入: 
+//
+// "bbbab"
+// 
+//
+// 输出: 
+//
+// 4
+// 
+//
+// 一个可能的最长回文子序列为 "bbbb"。 
+//
+// 示例 2: 
+//输入: 
+//
+// "cbbd"
+// 
+//
+// 输出: 
+//
+// 2
+// 
+//
+// 一个可能的最长回文子序列为 "bb"。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 只包含小写英文字母 
+// 
+// Related Topics 动态规划
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+//Java：最长回文子序列
+public class P516LongestPalindromicSubsequence{
+    public static void main(String[] args) {
+        Solution solution = new P516LongestPalindromicSubsequence().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+
+        /**
+         * dp[i][j]表示s[i:j]最长回文子序列
+         */
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = 1;
+        }
+
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][len - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
