d51e8aa5f15cabceca4b46466a07ca9798f57a4c
gongpulin <gongpulin@163.com>
2021-01-05 09:14:21 +0800

    三角形最小路径和

---
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P120Triangle.java
@@ -0,0 +1,60 @@
+//给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+//
+// 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+//
+//
+//
+// 例如，给定三角形：
+//
+// [
+//     [2],
+//    [3,4],
+//   [6,5,7],
+//  [4,1,8,3]
+//]
+// 
+//
+// 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。 
+//
+// 
+//
+// 说明： 
+//
+// 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。 
+// Related Topics 数组 动态规划
+
+
+package com.gpl.leetcode.leetcode.editor.cn;
+
+import java.util.List;
+
+//Java：三角形最小路径和
+public class P120Triangle{
+    public static void main(String[] args) {
+        Solution solution = new P120Triangle().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+            }
+            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+        int ans = dp[n - 1][0];
+        for (int i = 1; i < n; i++) {
+            ans = Math.min(ans, dp[n - 1][i]);
+        }
+        return ans;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
