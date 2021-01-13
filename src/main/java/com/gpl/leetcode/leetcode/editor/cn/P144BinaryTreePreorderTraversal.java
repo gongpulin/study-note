2fec27ef11a8513f5706199c049cccfd668d75ee
gongpulin <gongpulin@163.com>
2021-01-13 09:13:10 +0800

    二叉树的前序遍历

--- src/main/java/com/gpl/leetcode/leetcode/editor/cn/P144BinaryTreePreorderTraversal.java
+++ src/main/java/com/gpl/leetcode/leetcode/editor/cn/P144BinaryTreePreorderTraversal.java
@@ -40,9 +40,39 @@ public class P144BinaryTreePreorderTraversal{
  */
 class Solution {
 
-
-
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList();
+        if (root == null) {
+            return ans;
+        }
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public List<Integer> preorderTraversal2(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         if (root == null) {
             return ans;
